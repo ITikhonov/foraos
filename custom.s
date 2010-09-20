985d4e7cb4bbe9186c00e108e6fa2358cf124d2c
Ivan Tikhonov <kefeer@brokestream.com>
2010-09-20 19:21:41 +0600

    flashing backlight

---
+++ custom.s
@@ -0,0 +1,46 @@
+.section ".start", #alloc, #execinstr
+
+start:
+	mov r0,r0
+	mov r0,r0
+	mov r0,r0
+	mov r0,r0
+	mov r0,r0
+	mov r0,r0
+	mov r0,r0
+	mov r0,r0
+
+	b realstart
+	.word 0x016f2818
+	.word start
+	.word end
+
+
+realstart:
+	ldr	r4,[pc,$(COUNTER-.-8)]
+loop:
+	subs	r4,#1
+	bne	loop
+	b	backlight
+COUNTER:.word 0x2000000
+
+backlight:
+	ldr	r0,[pc,$(GPT10_CLR-.-8)]
+	ldr	r1,[r0]
+	eor	r1,#1
+	str	r1,[r0]
+	b	realstart
+GPT10_CLR:.word 0x48086024 
+
+
+reboot:
+	ldr	r0,[pc,$(PRCCTRL-.-8)]
+	mov	r1,#4
+	str	r1,[r0]
+	b halt
+PRCCTRL: .word 0x48307250
+
+
+halt:	b halt
+
+end:
